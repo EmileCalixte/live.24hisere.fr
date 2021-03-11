1b156c100aabf1c7058a82011f52cbc64d6ecfac
Emile Calixte <calixte.emile@gmail.com>
2021-03-11 21:11:57 +0100

    Backend project initialization

--- backend/www/index.php
+++ backend/www/index.php
@@ -1,3 +1,16 @@
 <?php
 
-echo "Hello world";
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Slim\Factory\AppFactory;
+
+require __DIR__ . '/../vendor/autoload.php';
+
+$app = AppFactory::create();
+
+$app->get('/', function (ServerRequestInterface $request, ResponseInterface $response, $args) {
+    $response->getBody()->write("Hello world!");
+    return $response;
+});
+
+$app->run();
