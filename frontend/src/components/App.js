b5a3527dbc45dccc303e39bddfef082253ca582d
Emile Calixte <calixte.emile@gmail.com>
2021-03-27 20:18:10 +0100

    Bootstrap-grid

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -4,9 +4,17 @@ import TestCanvas from "./TestCanvas";
 class App extends React.Component {
     render = () => {
         return (
-            <div className="App">
-                App
-                <TestCanvas/>
+            <div className="app">
+                <div className="container-fluid">
+                    <div className="row">
+                        <div className="col-sm">
+                            App
+                        </div>
+                        <div className="col-sm">
+                            <TestCanvas/>
+                        </div>
+                    </div>
+                </div>
             </div>
         );
     }
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './css/bootstrap-grid.min.css';
 import './css/index.css';
 import App from './components/App';
 
