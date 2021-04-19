462748ea2f9a1594277fe6c1dd3555f120ccb48c
Emile Calixte <calixte.emile@gmail.com>
2021-04-19 11:51:10 +0200

    Convert layout Class Components to Function Components

--- frontend/src/components/layout/footer/Footer.js
+++ frontend/src/components/layout/footer/Footer.js
@@ -1,11 +1,7 @@
-import React from "react";
-
-class Footer extends React.Component {
-    render = () => {
-        return(
-            <footer id="app-footer">Footer</footer>
-        )
-    }
+const Footer = () => {
+    return(
+        <footer id="app-footer">Footer</footer>
+    )
 }
 
 export default Footer;
