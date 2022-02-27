fdf3bb62f8d8904728c8e150534f121d79017bdf
Emile Calixte <calixte.emile@gmail.com>
2022-02-27 10:08:24 +0100

    Create runner laps table

--- frontend/src/components/pages/runner-details/RunnerDetailsLaps.js
+++ frontend/src/components/pages/runner-details/RunnerDetailsLaps.js
@@ -1,8 +1,67 @@
+import {useMemo} from "react";
+import Util from "../../../util/Util";
+
 const RunnerDetailsLaps = ({runner}) => {
-    console.log(runner);
+    const passagesToDisplay = useMemo(() => {
+        const passagesToDisplay = [...runner.passages];
+
+        return passagesToDisplay;
+    }, [runner]);
 
     return (
-        <h2>Détails des tours</h2>
+        <div className="row">
+            <div className="col-12">
+                <h2>Détails des tours</h2>
+
+                <div style={{maxWidth: 1400}}>
+                    <table id="runner-laps-table" className="table">
+                        <thead>
+                        <tr>
+                            <th>Nb. tours</th>
+                            <th>Distance</th>
+                            <th>Temps de course</th>
+                            <th>Temps au tour</th>
+                            <th>Vitesse</th>
+                            <th>Allure</th>
+                            <th>Vmoy. depuis le début</th>
+                            <th>Allure depuis le début</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        {passagesToDisplay.map((passage, index) => (
+                            <tr key={index}>
+                                <td>
+                                    {passage.processed.lapNumber !== null ? passage.processed.lapNumber : '–'}
+                                </td>
+                                <td>
+                                    {(passage.processed.totalDistance / 1000).toFixed(2)} km
+                                </td>
+                                <td>
+                                    {Util.formatMsAsDuration(passage.processed.lapEndRaceTime)}
+                                </td>
+                                <td>
+                                    {Util.formatMsAsDuration(passage.processed.lapDuration)}
+                                </td>
+                                <td>
+                                    {passage.processed.lapSpeed.toFixed(2)} km/h
+                                </td>
+                                <td>
+                                    {Util.formatMsAsDuration(passage.processed.lapPace, false)}/km
+                                </td>
+                                <td>
+                                    {passage.processed.averageSpeedSinceRaceStart.toFixed(2)} km/h
+                                </td>
+                                <td>
+                                    {Util.formatMsAsDuration(passage.processed.averagePaceSinceRaceStart, false)}/km
+                                </td>
+                            </tr>
+                        ))}
+                        </tbody>
+                    </table>
+                </div>
+
+            </div>
+        </div>
     )
 }
 
