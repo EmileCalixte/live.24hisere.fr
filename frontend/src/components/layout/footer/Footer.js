0dea66c2553ae2ed9e029b908e0bc5bdd2582cb8
Emile Calixte <calixte.emile@gmail.com>
2022-02-27 14:14:42 +0100

    Add footer

--- frontend/src/components/layout/footer/Footer.js
+++ frontend/src/components/layout/footer/Footer.js
@@ -1,6 +1,25 @@
+import {app} from "../../App";
+import Util from "../../../util/Util";
+
 const Footer = () => {
     return(
-        <footer id="app-footer">Footer</footer>
+        <footer id="app-footer">
+            <div className="container-fluid">
+                <div className="row">
+                    <div className="col-12" style={{textAlign: 'center'}}>
+                        <p>Dernière mise à jour des données : {Util.formatDateAsString(app.state.lastUpdateTime)}</p>
+
+                        <p>Toutes les données disponibles sur cette page sont extraites du système de chronométrage. Toutefois, ayant un but purement indicatif, les caclculs peuvent éventuellement contenir des erreurs ou des imprécisions. Seules les données du poste de chronométrage font foi.</p>
+
+                        <p>Application développée pour Les 24 Heures de l'Isère par Emile Calixte, avec l'aide de Djeson Pascal-Valette</p>
+
+                        <p>
+                            <a href="https://github.com/EmileCalixte/live.24hisere.fr" target="_blank">Code source</a> – <a href="https://www.24hisere.fr/" target="_blank">Les 24 Heures de l'Isère</a>
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </footer>
     )
 }
 
--- frontend/src/util/Util.js
+++ frontend/src/util/Util.js
@@ -28,6 +28,21 @@ class Util {
         return `${year}-${month}-${day}T${hours}:${minutes}:${seconds}`;
     }
 
+    static formatDateAsString = (date) => {
+        if (!(date instanceof Date)) {
+            throw new Error('date must be a Date');
+        }
+
+        const year = date.getFullYear();
+        const month = Util.prefixNumber(date.getMonth() + 1, 2);
+        const day = Util.prefixNumber(date.getDate(), 2);
+        const hours = Util.prefixNumber(date.getHours(), 2);
+        const minutes = Util.prefixNumber(date.getMinutes(), 2);
+        const seconds = Util.prefixNumber(date.getSeconds(), 2);
+
+        return `${day}/${month}/${year}, ${hours}:${minutes}:${seconds}`;
+    }
+
     static formatFloatNumber = (number, decimalsCount) => {
         return Number.parseFloat(number).toFixed(decimalsCount);
     }
