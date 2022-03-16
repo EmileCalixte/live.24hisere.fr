35871e8cf73d62125d93868185fde221c5e18570
Emile Calixte <calixte.emile@gmail.com>
2022-03-16 06:31:35 +0100

    Change log text

--- import/includes/retrieveData/GetDataFromUrl.ps1
+++ import/includes/retrieveData/GetDataFromUrl.ps1
@@ -1,5 +1,5 @@
 function GetDataFromUrl($url) {
-    Log "Récupération des données"
+    Log "Téléchargement des données"
 
     $response = Invoke-WebRequest -Uri $url -Method GET -UseBasicParsing
 
