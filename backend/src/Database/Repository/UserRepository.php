dc44e6fc541b8aefeb0ffdd46f4fb17e18c9f039
Emile Calixte <calixte.emile@gmail.com>
2022-06-08 21:12:28 +0200

    UsersResponder returns list of users

--- backend/src/Database/Repository/UserRepository.php
+++ backend/src/Database/Repository/UserRepository.php
@@ -5,9 +5,23 @@ namespace App\Database\Repository;
 use App\Database\Entity\User;
 use Doctrine\ORM\EntityRepository;
 use Doctrine\ORM\NoResultException;
+use Doctrine\ORM\Query;
 
 class UserRepository extends EntityRepository
 {
+    /**
+     * @param bool $asArray
+     * @return User[]|array The list of users
+     */
+    public function findAll(bool $asArray = false): array
+    {
+        $query = $this->createQueryBuilder('u')
+            ->orderBy('u.id', 'ASC')
+            ->getQuery();
+
+        return $query->getResult($asArray ? Query::HYDRATE_ARRAY : Query::HYDRATE_OBJECT);
+    }
+
     public function findById(int $id): ?User
     {
         $query = $this->createQueryBuilder('u')
--- backend/src/Responder/Admin/UsersResponder.php
+++ backend/src/Responder/Admin/UsersResponder.php
@@ -2,7 +2,11 @@
 
 namespace App\Responder\Admin;
 
+use App\Database\Entity\User;
+use App\Database\Repository\RepositoryProvider;
+use App\Misc\Util\CommonUtil;
 use App\Responder\AbstractResponder;
+use App\Security\Authentication\Authentication;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 
@@ -12,8 +16,27 @@ class UsersResponder extends AbstractResponder
     {
         $this->requireAuthentication($request);
 
-        // TODO return users
-        $response->getBody()->write("Access granted");
+        $userRepository = RepositoryProvider::getRepository(User::class);
+
+        $users = $userRepository->findAll(asArray: true);
+
+        $currentUser = Authentication::getInstance()->getUser();
+        foreach ($users as &$user) {
+            if ($user['id'] === $currentUser->getId()) {
+                $user['isCurrentUser'] = true;
+            } else {
+                $user['isCurrentUser'] = false;
+            }
+        }
+
+        $responseData = [
+            'users' => $users,
+        ];
+
+        CommonUtil::camelizeArrayKeysRecursively($responseData);
+
+        $response->getBody()->write(CommonUtil::jsonEncode($responseData));
+
         return $response;
     }
 }
