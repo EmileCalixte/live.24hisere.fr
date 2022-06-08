6f3bb9bb75440adbc67c5877b70916e599f790a1
Emile Calixte <calixte.emile@gmail.com>
2022-06-08 19:18:26 +0200

    Write requireAuthentication function in AbstractResponder

--- backend/src/Responder/AbstractResponder.php
+++ backend/src/Responder/AbstractResponder.php
@@ -4,12 +4,34 @@ namespace App\Responder;
 
 use App\Security\Authentication\Authentication;
 use Psr\Http\Message\ServerRequestInterface;
+use Slim\Exception\HttpForbiddenException;
+use Slim\Exception\HttpUnauthorizedException;
 
 abstract class AbstractResponder implements ResponderInterface
 {
-    protected function requireAuthentication(ServerRequestInterface $request) {
-        // The user authenticated with request header access token
-        dd(Authentication::getInstance()->getUser());
-        // TODO throw exception if user is not authenticated
+    protected function requireAuthentication(ServerRequestInterface $request)
+    {
+        $authentication = Authentication::getInstance();
+
+        if (!$authentication->authenticationOccurred()) {
+            throw new HttpForbiddenException($request, "Access token must be provided");
+        }
+
+        if (is_null($authentication->getUser())) {
+            $this->throwUnauthorizedException($authentication, $request);
+        }
+    }
+
+    private function throwUnauthorizedException(Authentication $authentication, ServerRequestInterface $request)
+    {
+        if ($authentication->isExpiredToken()) {
+            throw new HttpUnauthorizedException($request, "Access token has expired");
+        }
+
+        if ($authentication->isInvalidToken()) {
+            throw new HttpUnauthorizedException($request, "Access token is invalid");
+        }
+
+        throw new HttpUnauthorizedException($request, "Unable to authenticate");
     }
 }
