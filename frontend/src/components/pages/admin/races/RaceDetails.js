208ca16446335d1f27c32faf08906ba9cbe4204a
Emile Calixte <calixte.emile@gmail.com>
2022-11-07 20:36:09 +0100

    Create race details form

--- frontend/src/components/pages/admin/races/RaceDetails.js
+++ frontend/src/components/pages/admin/races/RaceDetails.js
@@ -1,15 +1,55 @@
 import {Navigate, useParams} from "react-router-dom";
 import Breadcrumbs from "../../../layout/breadcrumbs/Breadcrumbs";
 import Crumb from "../../../layout/breadcrumbs/Crumb";
-import {useCallback, useEffect, useState} from "react";
+import {useCallback, useEffect, useMemo, useState} from "react";
 import CircularLoader from "../../../misc/CircularLoader";
 import ApiUtil from "../../../../util/ApiUtil";
 import {app} from "../../../App";
+import Util from "../../../../util/Util";
 
 const RaceDetails = () => {
     const {raceId: urlRaceId} = useParams();
 
     const [race, setRace] = useState(undefined);
+
+    const [raceName, setRaceName] = useState(null);
+    const [initialDistance, setInitialDistance] = useState(null);
+    const [lapDistance, setLapDistance] = useState(null);
+    const [startTime, setStartTime] = useState(null);
+    const [isPublic, setIsPublic] = useState(null);
+
+    const [submitButtonDisabled, setSubmitButtonDisabled] = useState(false);
+
+    const startTimeDate = useMemo(() => {
+        if (!startTime) {
+            return;
+        }
+
+        // Date input value requires YYYY-MM-DD format
+        return Util.getDateStringFromDate(startTime, '-').split('-').reverse().join('-');
+    }, [startTime]);
+
+    const startTimeTime = useMemo(() => {
+        if (!startTime) {
+            return '';
+        }
+
+        return Util.getTimeStringFromDate(startTime);
+    }, [startTime]);
+
+    const unsavedChanges = useMemo(() => {
+        if (!race) {
+            return false;
+        }
+
+        return[
+            raceName === race.name,
+            initialDistance.toString() === race.initialDistance.toString(),
+            lapDistance.toString() === race.lapDistance.toString(),
+            startTime.getTime() === new Date(race.startTime).getTime(),
+            isPublic === race.isPublic,
+        ].includes(false);
+    }, [race, raceName, initialDistance, lapDistance, startTime, isPublic]);
 
     const fetchRace = useCallback(async () => {
         const response = await ApiUtil.performAuthenticatedAPIRequest(`/admin/races/${urlRaceId}`, app.state.accessToken);
@@ -23,13 +63,36 @@ const RaceDetails = () => {
         const responseJson = await response.json();
 
         setRace(responseJson.race);
+
+        setRaceName(responseJson.race.name);
+        setInitialDistance(responseJson.race.initialDistance);
+        setLapDistance(responseJson.race.lapDistance);
+        setStartTime(new Date(responseJson.race.startTime));
+        setIsPublic(responseJson.race.isPublic);
     }, [urlRaceId]);
 
     useEffect(() => {
         fetchRace();
     }, [fetchRace]);
 
-    console.log(race);
+    const onStartTimeDateChange = (e) => {
+        setStartTime(new Date(`${e.target.value}T${startTimeTime}`));
+    }
+
+    const onStartTimeTimeChange = (e) => {
+        setStartTime(new Date(`${startTimeDate}T${e.target.value}`));
+    }
+
+    const onSubmit = async (e) => {
+        e.preventDefault();
+        setSubmitButtonDisabled(true);
+
+        // TODO
+
+        setSubmitButtonDisabled(false);
+
+        fetchRace();
+    }
 
     if (race === null) {
         return (
@@ -59,13 +122,88 @@ const RaceDetails = () => {
                 </div>
             </div>
             <div className="row">
-                <div className="col-12">
+                <div className="col-xl-4 col-lg-6 col-md-9 col-12">
                     {race === undefined &&
                     <CircularLoader />
                     }
 
                     {race !== undefined &&
-                    race.name
+                    <form onSubmit={onSubmit}>
+                        <div className="input-group">
+                            <label>
+                                Nom
+                                <input className="input"
+                                       type="text"
+                                       value={raceName}
+                                       name="name"
+                                       onChange={(e) => setRaceName(e.target.value)}
+                                />
+                            </label>
+                        </div>
+
+                        <div className="input-group">
+                            <label>
+                                Distance avant premier passage (m)
+                                <input className="input"
+                                       type="number"
+                                       step={0.001}
+                                       value={initialDistance}
+                                       name="initial-distance"
+                                       onChange={(e) => setInitialDistance(e.target.value)}
+                                />
+                            </label>
+                        </div>
+
+                        <div className="input-group">
+                            <label>
+                                Distance du tour (m)
+                                <input className="input"
+                                       type="number"
+                                       step={0.001}
+                                       value={lapDistance}
+                                       name="initial-distance"
+                                       onChange={(e) => setLapDistance(e.target.value)}
+                                />
+                            </label>
+                        </div>
+
+                        <div className="input-group">
+                            <label>
+                                Départ
+                                <input className="input"
+                                       type="date"
+                                       defaultValue={startTimeDate}
+                                       name="start-date"
+                                       onChange={onStartTimeDateChange}
+                                />
+                                <input className="input"
+                                       type="time"
+                                       step={1}
+                                       defaultValue={startTimeTime}
+                                       name="start-time"
+                                       onChange={onStartTimeTimeChange}
+                                />
+                            </label>
+                        </div>
+
+                        <div className="inline-input-group mt-3">
+                            <label className="input-checkbox">
+                                <input type="checkbox"
+                                       checked={isPublic}
+                                       onChange={(e) => setIsPublic(e.target.checked)}
+                                />
+                                <span/>
+                                Visible par les utilisateurs
+                            </label>
+                        </div>
+
+                        <button className="button mt-3"
+                                type="submit"
+                                disabled={submitButtonDisabled || !unsavedChanges}
+                        >
+                            Enregistrer
+                        </button>
+                    </form>
                     }
                 </div>
             </div>
