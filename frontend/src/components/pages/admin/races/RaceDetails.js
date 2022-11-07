8fd916dfdea54ec4a430ac5c26327b3b8036b84c
Emile Calixte <calixte.emile@gmail.com>
2022-11-07 18:36:03 +0100

    Fetch race in admin race details page

--- frontend/src/components/pages/admin/races/RaceDetails.js
+++ frontend/src/components/pages/admin/races/RaceDetails.js
@@ -1,11 +1,41 @@
-import {useParams} from "react-router-dom";
+import {Navigate, useParams} from "react-router-dom";
 import Breadcrumbs from "../../../layout/breadcrumbs/Breadcrumbs";
 import Crumb from "../../../layout/breadcrumbs/Crumb";
+import {useCallback, useEffect, useState} from "react";
+import CircularLoader from "../../../misc/CircularLoader";
+import ApiUtil from "../../../../util/ApiUtil";
+import {app} from "../../../App";
 
 const RaceDetails = () => {
     const {raceId: urlRaceId} = useParams();
 
-    console.log(urlRaceId);
+    const [race, setRace] = useState(undefined);
+
+    const fetchRace = useCallback(async () => {
+        const response = await ApiUtil.performAuthenticatedAPIRequest(`/admin/races/${urlRaceId}`, app.state.accessToken);
+
+        if (!response.ok) {
+            console.error('Failed to fetch race', await response.json());
+            setRace(null);
+            return;
+        }
+
+        const responseJson = await response.json();
+
+        setRace(responseJson.race);
+    }, [urlRaceId]);
+
+    useEffect(() => {
+        fetchRace();
+    }, [fetchRace]);
+
+    console.log(race);
+
+    if (race === null) {
+        return (
+            <Navigate to="/admin/races" />
+        );
+    }
 
     return (
         <div id="page-admin-race-details">
@@ -14,13 +44,29 @@ const RaceDetails = () => {
                     <Breadcrumbs>
                         <Crumb url="/admin" label="Administration" />
                         <Crumb url="/admin/races" label="Courses" />
-                        <Crumb label={urlRaceId} /> {/*TODO once race fetched, use name instead of ID*/}
+                        {(() => {
+                            if (race === undefined) {
+                                return (
+                                    <CircularLoader />
+                                )
+                            }
+
+                            return (
+                                <Crumb label={race.name}/>
+                            )
+                        })()}
                     </Breadcrumbs>
                 </div>
             </div>
             <div className="row">
                 <div className="col-12">
-                    {urlRaceId}
+                    {race === undefined &&
+                    <CircularLoader />
+                    }
+
+                    {race !== undefined &&
+                    race.name
+                    }
                 </div>
             </div>
         </div>
