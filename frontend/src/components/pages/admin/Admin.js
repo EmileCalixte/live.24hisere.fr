092a22cde4180fb5aaccdaca3fbd2ed25a52c78b
Emile Calixte <calixte.emile@gmail.com>
2022-11-07 14:07:08 +0100

    Display loader in admin section while fetching user info

--- frontend/src/components/pages/admin/Admin.js
+++ frontend/src/components/pages/admin/Admin.js
@@ -3,12 +3,19 @@ import {Navigate, Route, Routes} from "react-router-dom";
 import AdminHome from "./home/AdminHome";
 import Runners from "./runners/Runners";
 import RaceSettings from "./race-settings/RaceSettings";
+import CircularLoader from "../../misc/CircularLoader";
 
 const Admin = () => {
     if (app.state.user === null) {
         return (
             <Navigate to="/" />
         );
+    }
+
+    if (app.state.user === undefined) {
+        return (
+            <CircularLoader />
+        )
     }
 
     return (
