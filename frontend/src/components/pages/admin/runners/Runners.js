8f947ece80732481ee112dda575abbd40ae2558f
Emile Calixte <calixte.emile@gmail.com>
2022-11-14 21:50:06 +0100

    Display runners list in admin panel

--- frontend/src/components/pages/admin/runners/Runners.js
+++ frontend/src/components/pages/admin/runners/Runners.js
@@ -1,7 +1,31 @@
 import Breadcrumbs from "../../../layout/breadcrumbs/Breadcrumbs";
 import Crumb from "../../../layout/breadcrumbs/Crumb";
+import {useCallback, useEffect, useState} from "react";
+import ApiUtil from "../../../../util/ApiUtil";
+import {app} from "../../../App";
+import CircularLoader from "../../../misc/CircularLoader";
+import {Link} from "react-router-dom";
+import RunnersTable from "./RunnersTable";
 
 const Runners = () => {
+    // false = not fetched yet. Once fetched, it's an array
+    const [runners, setRunners] = useState(false);
+
+    // false = not fetched yet. Once fetched, it's an object
+    const [races, setRaces] = useState(false);
+
+    const fetchRunnersAndRaces = useCallback(async () => {
+        const response = await ApiUtil.performAuthenticatedAPIRequest('/admin/runners', app.state.accessToken);
+        const responseJson = await response.json();
+
+        setRunners(responseJson.runners);
+        setRaces(responseJson.races);
+    }, []);
+
+    useEffect(() => {
+        fetchRunnersAndRaces();
+    }, [fetchRunnersAndRaces]);
+
     return (
         <div id="page-admin-runners">
             <div className="row">
@@ -12,6 +36,31 @@ const Runners = () => {
                     </Breadcrumbs>
                 </div>
             </div>
+
+            {runners === false &&
+            <CircularLoader />
+            }
+
+            {runners !== false &&
+            <div className="row">
+                <div className="col-12">
+                    <Link to="/admin/runners/create" className="button">
+                        <i className="fa-solid fa-plus mr-2"/>
+                        Ajouter un coureur
+                    </Link>
+                </div>
+
+                <div className="col-12 mt-3">
+                    {runners.length === 0 &&
+                    <p>Aucun coureur</p>
+                    }
+
+                    {runners.length > 0 &&
+                    <RunnersTable runners={runners} races={races}/>
+                    }
+                </div>
+            </div>
+            }
         </div>
     )
 }
