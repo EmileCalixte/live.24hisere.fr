722f79a4558ff482890d2ad195156a2c48cb7e57
Emile Calixte <calixte.emile@gmail.com>
2022-11-20 19:40:03 +0100

    Convert OptionWithLoadingDots to typescript & modernize code

--- frontend/src/components/misc/OptionWithLoadingDots.tsx
+++ frontend/src/components/misc/OptionWithLoadingDots.tsx
@@ -1,12 +1,15 @@
-import {useEffect, useState} from "react"
+import React, {useCallback, useEffect, useMemo, useState} from "react"
 
 export const MIN_DOTS = 0;
 export const MAX_DOTS = 3;
 export const UPDATE_DOT_COUNT_INTERVAL_TIME = 250;
 
-const OptionWithLoadingDots = ({children}) => {
+const OptionWithLoadingDots: React.FunctionComponent<{children: React.ReactNode}> = ({children}) => {
     const [dotCount, setDotCount] = useState(MAX_DOTS);
-    const updateDotCount = () => setDotCount(dotCount => dotCount >= MAX_DOTS ? MIN_DOTS : dotCount + 1);
+
+    const updateDotCount = useCallback(() => {
+        setDotCount(dotCount => dotCount >= MAX_DOTS ? MIN_DOTS : dotCount + 1)
+    }, [dotCount]);
 
     useEffect(() => {
         const dotsInterval = setInterval(() => updateDotCount(), UPDATE_DOT_COUNT_INTERVAL_TIME);
@@ -14,7 +17,7 @@ const OptionWithLoadingDots = ({children}) => {
         return (() => clearInterval(dotsInterval));
     }, []);
 
-    const getDots = () => {
+    const dots = useMemo<string>(() => {
         const dots = [];
 
         for (let i = 0; i < dotCount; ++i) {
@@ -22,11 +25,11 @@ const OptionWithLoadingDots = ({children}) => {
         }
 
         return dots.join('');
-    };
+    }, [dotCount]);
 
     return (
         <option disabled>
-            {children}{getDots()}
+            {children}{dots}
         </option>
     );
 }
