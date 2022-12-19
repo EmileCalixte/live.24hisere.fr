4ddd7322e87a72444cfaf2d83902a3a7799b46f9
Emile Calixte <calixte.emile@gmail.com>
2022-12-19 11:52:00 +0100

    Fetch dag file content

--- backend/src/Command/Cron/ImportPassagesCronCommand.php
+++ backend/src/Command/Cron/ImportPassagesCronCommand.php
@@ -2,6 +2,10 @@
 
 namespace App\Command\Cron;
 
+use App\Database\Entity\Config;
+use App\Database\Repository\ConfigRepository;
+use App\Database\Repository\RepositoryProvider;
+use Curl\Curl;
 use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
@@ -15,7 +19,28 @@ class ImportPassagesCronCommand extends Command
 {
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        $output->writeln('TODO import passages');
+        /** @var ConfigRepository $configRepository */
+        $configRepository = RepositoryProvider::getRepository(Config::class);
+
+        $dagFilePath = $configRepository->getKeyValue(Config::KEY_IMPORT_DAG_FILE_PATH);
+
+        if (is_null($dagFilePath)) {
+            $output->writeln('Dag file path is not defined');
+            return Command::FAILURE;
+        }
+
+        $curl = (new Curl())->get($dagFilePath);
+
+        if (!$curl->isSuccess()) {
+            $output->writeln("Curl error: $curl->curl_error_message ($curl->error_code)");
+            return Command::FAILURE;
+        }
+
+        $dagFileContent = $curl->response;
+
+        $curl->close();
+
+        var_dump($dagFileContent);
 
         return Command::SUCCESS;
     }
