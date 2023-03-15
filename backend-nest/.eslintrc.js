8e73a736d4892bda0f89b3ef51e4c2c898bcddd8
Emile Calixte <calixte.emile@gmail.com>
2023-03-15 22:54:13 +0100

    Configure ESLint

--- backend-nest/.eslintrc.js
+++ backend-nest/.eslintrc.js
@@ -1,25 +1,85 @@
 module.exports = {
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    project: 'tsconfig.json',
-    tsconfigRootDir: __dirname,
-    sourceType: 'module',
-  },
-  plugins: ['@typescript-eslint/eslint-plugin'],
-  extends: [
-    'plugin:@typescript-eslint/recommended',
-    'plugin:prettier/recommended',
-  ],
-  root: true,
-  env: {
-    node: true,
-    jest: true,
-  },
-  ignorePatterns: ['.eslintrc.js'],
-  rules: {
-    '@typescript-eslint/interface-name-prefix': 'off',
-    '@typescript-eslint/explicit-function-return-type': 'off',
-    '@typescript-eslint/explicit-module-boundary-types': 'off',
-    '@typescript-eslint/no-explicit-any': 'off',
-  },
+    parser: "@typescript-eslint/parser",
+    parserOptions: {
+        project: "tsconfig.json",
+        tsconfigRootDir: __dirname,
+        sourceType: "module",
+    },
+    plugins: ["@typescript-eslint/eslint-plugin"],
+    extends: [
+        "plugin:@typescript-eslint/recommended",
+    ],
+    root: true,
+    env: {
+        node: true,
+        jest: true,
+    },
+    ignorePatterns: [],
+    rules: {
+        "@typescript-eslint/indent": ["error", 4, {
+            SwitchCase: 1,
+        }],
+
+        "@typescript-eslint/quotes": ["error", "double"],
+
+        "@typescript-eslint/semi": ["error", "always"],
+
+        "no-extra-semi": "error",
+
+        "@typescript-eslint/comma-dangle": ["error", "always-multiline"],
+
+        "object-curly-spacing": ["error", "never"],
+        "@typescript-eslint/object-curly-spacing": ["error", "never"],
+
+        "@typescript-eslint/naming-convention": [
+            "error",
+            {
+                selector: "variable",
+                format: ["camelCase", "UPPER_CASE", "PascalCase"],
+            },
+            {
+                selector: "parameter",
+                format: ["camelCase", "PascalCase"],
+                leadingUnderscore: "allow",
+            },
+            {
+                selector: "memberLike",
+                modifiers: ["private"],
+                format: ["camelCase", "PascalCase"],
+            },
+            {
+                selector: "typeLike",
+                format: ["PascalCase"],
+            },
+        ],
+
+        "@typescript-eslint/member-delimiter-style": ["error", {
+            multiline: {
+                delimiter: "semi",
+                requireLast: true,
+            },
+            singleline: {
+                delimiter: "semi",
+                requireLast: false,
+            },
+            multilineDetection: "brackets",
+        }],
+
+        "@typescript-eslint/no-empty-function": ["error", {allow: ["arrowFunctions"]}],
+
+        "@typescript-eslint/space-before-function-paren": ["error", {
+            anonymous: "always",
+            named: "never",
+            asyncArrow: "always",
+        }],
+
+        eqeqeq: ["error", "always"],
+
+        "brace-style": ["error", "1tbs", {allowSingleLine: true}],
+
+        "@typescript-eslint/interface-name-prefix": "off",
+        "@typescript-eslint/explicit-function-return-type": "off",
+        "@typescript-eslint/explicit-module-boundary-types": "off",
+        "@typescript-eslint/no-explicit-any": "off",
+    },
 };
