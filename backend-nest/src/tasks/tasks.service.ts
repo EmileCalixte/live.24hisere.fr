59e1e52dbc722bd2ab40c162ab5547d201269700
Emile Calixte <calixte.emile@gmail.com>
2023-04-08 13:44:37 +0200

    Add logger in Tasks

--- backend-nest/src/tasks/tasks.service.ts
+++ backend-nest/src/tasks/tasks.service.ts
@@ -1,12 +1,13 @@
-import {Injectable} from "@nestjs/common";
+import {Injectable, Logger} from "@nestjs/common";
 import {Cron} from "@nestjs/schedule";
 
 @Injectable()
 export class TasksService {
+    private readonly logger = new Logger("Tasks");
 
     @Cron("0,20,40 * * * * *")
     // @Interval(20000)
     testCron() {
-        console.log("Test cron", new Date());
+        this.logger.log(`Test cron ${new Date().toISOString()}`);
     }
 }
