bff91882e430c859dd4e581c246efc78fd5771b3
Emile Calixte <calixte.emile@gmail.com>
2023-04-15 23:33:45 +0200

    Remove old fontawesome lib files

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -3,7 +3,6 @@ import {createRoot} from "react-dom/client";
 import "bootstrap/dist/css/bootstrap-grid.css";
 import "./css/index.css";
 import "./css/forms.css";
-import "./lib/fontawesome/css/all.min.css";
 import "toastr2/dist/toastr.min.css";
 import "./css/toastr-override.css";
 import App from "./components/App";
