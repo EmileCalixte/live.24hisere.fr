8f9b9741865d2eed4e2f8833d4e0913fe1dfe743
Emile Calixte <calixte.emile@gmail.com>
2023-06-01 21:15:26 +0200

    Add CORS headers

--- backend-nest/src/main.ts
+++ backend-nest/src/main.ts
@@ -5,6 +5,11 @@ import {AppModule} from "./app.module";
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
     app.useGlobalPipes(new ValidationPipe());
+    app.enableCors({
+        methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
+        origin: process.env.NODE_ENV === "development" ? "*" : process.env.FRONTEND_URL ?? " ",
+        allowedHeaders: ["Authorization"],
+    });
     await app.listen(3000);
 }
 
