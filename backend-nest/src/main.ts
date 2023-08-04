203429c4e0ee5d3bf632e30938e636b911cad83e
Emile Calixte <calixte.emile@gmail.com>
2023-08-04 18:56:01 +0200

    Return 422 status instead of 400 for DTO errors

--- backend-nest/src/main.ts
+++ backend-nest/src/main.ts
@@ -1,13 +1,18 @@
-import { ValidationPipe } from "@nestjs/common";
+import { HttpStatus, ValidationPipe } from "@nestjs/common";
 import { NestFactory } from "@nestjs/core";
 import { useContainer } from "class-validator";
 import { AppModule } from "./app.module";
 
 async function bootstrap(): Promise<void> {
     const app = await NestFactory.create(AppModule);
+
     app.useGlobalPipes(new ValidationPipe({
-        whitelist: true, // Prevent unwanted properties in request body by only taking into account properties listed in DTOs
+        errorHttpStatusCode: HttpStatus.UNPROCESSABLE_ENTITY,
+
+        // Prevent unwanted properties in request body by only taking into account properties listed in DTOs
+        whitelist: true,
     }));
+
     app.enableCors({
         methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
         origin: process.env.NODE_ENV === "development" ? "*" : process.env.FRONTEND_URL ?? " ",
