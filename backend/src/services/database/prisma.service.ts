6f04d6ca2054c9fcd0ac212001ae17529ea2ddc4
Emile Calixte <calixte.emile@gmail.com>
2023-09-04 22:08:10 +0200

    Upgrade to Prisma 5

--- backend/src/services/database/prisma.service.ts
+++ backend/src/services/database/prisma.service.ts
@@ -8,7 +8,7 @@ export class PrismaService extends PrismaClient implements OnModuleInit {
     }
 
     async enableShutdownHooks(app: INestApplication): Promise<void> {
-        this.$on("beforeExit", () => {
+        process.on("beforeExit", () => {
             void app.close();
         });
     }
