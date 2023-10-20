f6979b1e65dc32e88f63f1bf1fa128b67d16a2a3
Emile Calixte <calixte.emile@gmail.com>
2023-10-20 07:54:02 +0200

    Update that triggers a build failure

--- frontend/src/components/viewParts/admin/races/RaceDetailsForm.tsx
+++ frontend/src/components/viewParts/admin/races/RaceDetailsForm.tsx
@@ -37,7 +37,7 @@ export default function RaceDetailsForm({
     setIsPublic,
     submitButtonDisabled,
 }: RaceDetailsFormProps): React.ReactElement {
-    const startTimeDate = useMemo(() => {
+    const startTimeDate = useMemo<string>(() => { // This should trigger a build failure
         if (!startTime) {
             return;
         }
