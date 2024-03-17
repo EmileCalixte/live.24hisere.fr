d4882b9c9e917603136cea0baeb9cc7f3ec6aee7
Emile Calixte <calixte.emile@gmail.com>
2024-03-17 20:32:51 +0100

    Oops

--- backend/src/types/Passage.ts
+++ backend/src/types/Passage.ts
@@ -3,7 +3,7 @@ import { type Passage } from "@prisma/client";
 /**
  * Public data of a passage
  */
-export type PublicPassage = Pick<Passage, "id" | "time" | "importTime">;
+export type PublicPassage = Pick<Passage, "id" | "time">;
 
 /**
  * Admin data of a runner's passage
