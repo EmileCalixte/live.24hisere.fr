57f5559bae1440d856cbe1e3030d2e760b704d67
Emile Calixte <calixte.emile@gmail.com>
2024-09-29 13:48:31 +0200

    Update dev data sql to generate DB with drizzle migrations

--- sql/01-init.sql
+++ sql/01-init.sql
@@ -1,7 +1,3 @@
 -- Create Prisma shadow database
 CREATE DATABASE IF NOT EXISTS `prisma_shadow`;
 GRANT ALL PRIVILEGES ON `prisma_shadow`.* TO 'admin'@'%';
-
--- Temporary: Create specific database for Drizzle
-CREATE DATABASE IF NOT EXISTS `live_drizzle`;
-GRANT ALL PRIVILEGES ON `live_drizzle`.* TO 'admin'@'%';
