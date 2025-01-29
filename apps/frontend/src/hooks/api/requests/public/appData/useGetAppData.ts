886fc438abab8ab2118a52f9300cc363f22ab312
Emile Calixte <calixte.emile@gmail.com>
2025-01-29 22:18:06 +0100

    use FETCH_INTERVAL const for app data fetching

--- apps/frontend/src/hooks/api/requests/public/appData/useGetAppData.ts
+++ apps/frontend/src/hooks/api/requests/public/appData/useGetAppData.ts
@@ -1,14 +1,12 @@
 import { useQuery } from "@tanstack/react-query";
+import { FETCH_INTERVAL } from "../../../../../constants/api";
 import { getAppData } from "../../../../../services/api/appDataService";
-
-// Fetch app data every 20 seconds
-const FETCH_APP_DATA_INTERVAL_TIME = 20 * 1000;
 
 export function useGetAppData() {
   return useQuery({
     queryKey: ["getAppData"],
     queryFn: getAppData,
-    refetchInterval: FETCH_APP_DATA_INTERVAL_TIME,
+    refetchInterval: FETCH_INTERVAL,
     retry: false,
   });
 }
