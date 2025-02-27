3c8f7c1a3f5b60b7b9a7e5746f0eed753cdccbec
Emile Calixte <calixte.emile@gmail.com>
2025-02-27 13:53:25 +0100

    Add immediate stop column in admin race view table

--- apps/frontend/src/components/views/admin/races/RacesAdminView.tsx
+++ apps/frontend/src/components/views/admin/races/RacesAdminView.tsx
@@ -50,6 +50,7 @@ export default function RacesAdminView(): React.ReactElement {
                       <th>Nb. coureurs</th>
                       <th>Publique</th>
                       <th>Class. simplifié</th>
+                      <th>Arrêt immédiat</th>
                       <th>Date</th>
                       <th>Durée</th>
                       <th>Distance</th>
@@ -91,6 +92,7 @@ export default function RacesAdminView(): React.ReactElement {
                             )}
                           </td>
                           <td>{race.isBasicRanking ? "Oui" : "Non"}</td>
+                          <td>{race.isImmediateStop ? "Oui" : "Non"}</td>
                           <td>{formatDateAsString(new Date(race.startTime))}</td>
                           <td>{formatMsAsDuration(race.duration * 1000)}</td>
                           <td>
