7b23ef1b668685f6f92024f7a87ff1b171378d55
Emile Calixte <calixte.emile@gmail.com>
2025-03-16 08:49:43 +0100

    fix no-unnecessary-type-parameters eslint error

--- apps/frontend/src/hooks/runners/useNameSortedRunners.ts
+++ apps/frontend/src/hooks/runners/useNameSortedRunners.ts
@@ -2,10 +2,9 @@ import React from "react";
 import type { PublicRunner } from "@live24hisere/core/types";
 import { spaceshipRunnersByName } from "../../utils/runnerUtils";
 
-export function useNameSortedRunners<
-  TRunner extends PublicRunner,
-  TRunners extends TRunner[] | null | undefined | false,
->(runners: TRunners): TRunners {
+export function useNameSortedRunners<TRunners extends PublicRunner[] | null | undefined | false>(
+  runners: TRunners,
+): TRunners {
   // @ts-ignore We know that the returned type is the same as the input type
   return React.useMemo(() => {
     if (!Array.isArray(runners)) {
