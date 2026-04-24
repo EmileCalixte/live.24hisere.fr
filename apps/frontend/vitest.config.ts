2159ff93acc94241855250a0fe9281a1c073049e
Emile Calixte <calixte.emile@gmail.com>
2026-04-24 08:16:52 +0200

    fix(vitest): use defaultExclude instead of replacing exclude array in vitest config

--- apps/frontend/vitest.config.ts
+++ apps/frontend/vitest.config.ts
@@ -1,9 +1,9 @@
-import { defineConfig } from "vitest/config";
+import { defaultExclude, defineConfig } from "vitest/config";
 
 export default defineConfig({
   test: {
     environment: "jsdom",
     setupFiles: "./test/setup.ts",
-    exclude: ["test-e2e/**", "node_modules/**"],
+    exclude: [...defaultExclude, "test-e2e/**"],
   },
 });
